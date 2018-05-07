3fb0839a5549528c1fc81e6323a2334ba64b8211
Albert Sare <thesundaymailman@gmail.com>
2018-05-07 12:30:31 -0700

    reached step 7

--- drills.js
+++ drills.js
@@ -1,8 +1,25 @@
-function whoAmI(name, age) {
+'use strict';
 
-	let yearOfBirth = 2018 - age;
+function whoAmI(name, age) {
+    let yob = yearOfBirth(age);
 
     console.log("Hello my name is Courtney and I'm 25 years old");
-    console.log(`I was born in ${yearOfBirth}.`);
+    console.log(`I was born in ${yob}.`);
+
 }
-whoAmI('Steve', 29);
+
+whoAmI('Steve', -5);
+
+function yearOfBirth(age) {
+    try {
+        if(age < 0) {
+            throw new Error('something');
+        }
+    }
+    catch(err) {
+        console.error(err.message);
+        console.log('this should run');
+    }
+
+	return 2018 - age;
+}
