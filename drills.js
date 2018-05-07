3326e9b11eec2bbecdec5ef4bba88ce0f75daca0
Courtney Adams <cadams8238@gmail.com>
2018-05-07 12:07:48 -0700

    added parameters and added yearOfBirth variable

--- drills.js
+++ drills.js
@@ -1,4 +1,8 @@
-function whoAmI() {
+function whoAmI(name, age) {
+
+	let yearOfBirth = 2018 - age;
+
     console.log("Hello my name is Courtney and I'm 25 years old");
+    console.log(`I was born in ${yearOfBirth}.`);
 }
-whoAmI();
+whoAmI('Steve', 29);
