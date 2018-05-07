a52c39c5ede6a2b92b49d9ff8f102ef00a3a412e
Courtney Adams <cadams8238@gmail.com>
2018-05-07 12:48:20 -0700

    added error checking on age and name parameters

--- drills.js
+++ drills.js
@@ -1,6 +1,21 @@
 'use strict';
 
 function whoAmI(name, age) {
+
+	try {
+		if (typeof name !== 'string') {
+			throw new Error('Invalid name argument, please insert a string');
+		}
+
+		if (typeof age !== 'number') {
+			throw new Error('Invalid age argument, please insert a number');
+		}
+	}
+	catch(err) {
+		console.error(err.message);
+	}
+	
+
     let yob = yearOfBirth(age);
 
     console.log("Hello my name is Courtney and I'm 25 years old");
@@ -8,17 +23,17 @@ function whoAmI(name, age) {
 
 }
 
-whoAmI('Steve', -5);
+whoAmI('Steve', 'Albert');
 
 function yearOfBirth(age) {
     try {
         if(age < 0) {
-            throw new Error('something');
+            throw new Error('Age cannot be negative');
         }
     }
     catch(err) {
-        console.error(err.message);
-        console.log('this should run');
+        console.error('Arguments not valid');
+      
     }
 
 	return 2018 - age;
